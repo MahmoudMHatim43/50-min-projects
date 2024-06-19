55ad73c2a06a6555d6b748cfbffce866b49b4f51
MahmoudMHatim43 <mahmoudmhatim@gmail.com>
2024-06-19 12:15:53 +0300

    👌 updated day 8 👌 added an eye to view password if needed

--- day_8_form/script.js
+++ day_8_form/script.js
@@ -1,5 +1,6 @@
 const icons = document.querySelectorAll(".icon");
 const fields = document.querySelectorAll(".field");
+const togglePassword = document.querySelector(".fa-eye-slash");
 
 fields.forEach((field) => {
 	field.addEventListener("input", () => {
@@ -7,4 +8,18 @@ fields.forEach((field) => {
 			icon.classList.add("start");
 		});
 	});
+});
+
+togglePassword.addEventListener("click", () => {
+	const passwordField = document.querySelector("#user_password");
+	if (passwordField.type === "password") {
+		passwordField.type = "text";
+		togglePassword.classList.remove("fa-eye-slash");
+		togglePassword.classList.add("fa-eye");
+		console.log(togglePassword);
+	} else {
+		passwordField.type = "password";
+		togglePassword.classList.remove("fa-eye");
+		togglePassword.classList.add("fa-eye-slash");
+	}
 });
