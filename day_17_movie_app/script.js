eefa6b33d106dea1a16470f3a82a80b1c3f9de3f
MahmoudMHatim43 <mahmoudmhatim@gmail.com>
2024-06-24 03:48:23 +0300

    updated version the old one used to have bugs but still even this version needs more work

--- day_17_movie_app/script.js
+++ day_17_movie_app/script.js
@@ -1,64 +1,69 @@
-const API_URL =
-	"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=af5e7fd345d26ed4958fa3ec67dab3b6&page=1";
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCH_API =
-	"https://api.themoviedb.org/3/search/movie?api_key=af5e7fd345d26ed4958fa3ec67dab3b6&query='";
+// variables used for connecting with the API
+const myApiKey = "af5e7fd345d26ed4958fa3ec67dab3b6"; // my key from tmdb
+const searchApi =
+	"https://api.themoviedb.org/3/search/movie?api_key=" +
+	myApiKey +
+	"&query='"; // &qurery=' to add our value of the search box;
+const mostPopularMovies_ApiUrl =
+	"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" +
+	myApiKey +
+	"&page=1";
+// const newMovies_ApiUrl = "";
+const imagesPath = "https://image.tmdb.org/t/p/w1280";
+// variables used with the html
+const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-const main = document.getElementById("main");
+const movieTitle = document.querySelector(".movie_title");
+const movieRate = document.querySelector(".movie_rate");
+const movieOverview = document.querySelector(".movie_overview");
 
-getMovies(API_URL);
+// get most popular movies by default:
+getMovies(mostPopularMovies_ApiUrl);
 async function getMovies(url) {
 	const res = await fetch(url);
 	const data = await res.json();
+	// append results
 	showMovies(data.results);
-	console.log(data.results);
 }
-
 function showMovies(movies) {
 	main.innerHTML = "";
 	movies.forEach((movie) => {
-		const { title, poster_path, vote_average, overview } = movie;
-
+		const { title, overview, poster_path, vote_average } = movie;
 		const movieEl = document.createElement("div");
-		movieEl.classList.add("movie");
-		movieEl.innerHTML = `<div class="movie">
-      <div class="movie_img">
-        <img
-          src= "${IMG_PATH + poster_path}"
-          alt="${title}">
-      </div>
-      <div class="movie_info">
-        <h3>${title}</h3>
-        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-      </div>
-      <div class="movie_overview">
-        <h3>Overview</h3>
-        <span class="overview">
-          ${overview}
-        </span>
-      </div>
-    </div>`;
-
+		movieEl.classList.add("movie_card");
+		movieEl.innerHTML = `
+        <div class="movie_image">
+        <img src="${imagesPath + poster_path}" width="100%"
+          alt="movie cover">
+        </div>
+        <h3 class="movie_title">${title}</h3>
+        <div class="movie_rate ${rateColor(
+			vote_average
+		)}">${vote_average.toFixed(1)}</div>
+        <div class="movie_overview">
+        <h3>overview</h3>
+        ${overview}
+        </div>`;
+		function rateColor(rate) {
+			if (rate >= 8) {
+				return "green";
+			} else if (rate >= 6) {
+				return "orange";
+			} else {
+				return "red";
+			}
+		}
 		main.appendChild(movieEl);
 	});
 }
 
-function getClassByRate(vote) {
-	if (vote >= 8) {
-		return "green";
-	} else if (vote >= 5) {
-		return "orange";
-	} else {
-		return "red";
-	}
-}
-
+// searching
 form.addEventListener("submit", (eve) => {
 	eve.preventDefault();
 	const searchTerm = search.value;
 	if (searchTerm && searchTerm !== "") {
-		getMovies(SEARCH_API + searchTerm);
+		getMovies(searchApi + searchTerm);
 		search.value = "";
 	} else {
 		window.location.reload();
