73e0774a6cbcc72bcbf78b2762c8ef0c41c0f04f
MahmoudMHatim43 <mahmoudmhatim@gmail.com>
2024-06-30 15:58:58 +0300

    ✔✔✔

--- day_35_image_carousel/script.js
+++ day_35_image_carousel/script.js
@@ -4,15 +4,18 @@ const rightBtn = document.querySelector("#right");
 const images = document.querySelectorAll("img");
 
 let x = 0;
+let length = (images.length - 1) * 100;
 leftBtn.addEventListener("click", () => {
-	if (x == 0) {
-		x = -images.length * 100;
+	x += 100;
+	if (x > 0) {
+		x = -length;
 	}
-	imageContainer.style.transform = `translateX(${(x += 100)}%)`;
+	imageContainer.style.transform = `translateX(${x}%)`;
 });
 rightBtn.addEventListener("click", () => {
-	if (x == -(images.length - 1) * 100) {
-		x = 100;
+	x -= 100;
+	if (x < length) {
+		x = 0;
 	}
-	imageContainer.style.transform = `translateX(${(x -= 100)}%)`;
+	imageContainer.style.transform = `translateX(${x}%)`;
 });
