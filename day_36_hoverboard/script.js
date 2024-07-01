fd1037a8c679149ba6aeee7bea031fc68fe01150
MahmoudMHatim43 <mahmoudmhatim@gmail.com>
2024-07-01 07:35:42 +0300

    ✔✔✔

--- day_36_hoverboard/script.js
+++ day_36_hoverboard/script.js
@@ -1,12 +1,15 @@
 const container = document.querySelector(".container");
 const input = document.querySelector("input");
+
 input.addEventListener("keydown", event => {
 	if (event.keyCode === 13) {
 		const value = event.target.value;
+
 		let i = 0;
 		while (i < value) {
 			const box = document.createElement("div");
 			box.classList.add("box");
+
 			box.addEventListener("mouseover", () => {
 				box.style.backgroundColor = `rgb(${Math.floor(Math.random() * 1000)}, ${Math.floor(
 					Math.random() * 1000
@@ -15,6 +18,7 @@ input.addEventListener("keydown", event => {
 			box.addEventListener("mouseout", () => {
 				box.style.backgroundColor = `rgb(52, 52, 52)`;
 			});
+
 			container.appendChild(box);
 			i++;
 		}
