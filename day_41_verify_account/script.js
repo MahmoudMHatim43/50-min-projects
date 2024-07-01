0bd62fc4fa73f91bb0d10a2e00e622cf3ba78357
MahmoudMHatim43 <mahmoudmhatim@gmail.com>
2024-07-01 22:02:51 +0300

    ✔✔✔

---
+++ day_41_verify_account/script.js
@@ -0,0 +1,22 @@
+const inputs = document.querySelectorAll("input");
+const button = document.querySelector(".submit");
+
+window.addEventListener("load", () => {
+	inputs[0].focus();
+});
+
+inputs.forEach((input, index) => {
+	input.addEventListener("keydown", event => {
+		if (event.key >= 0 && event.key < 10) {
+			if (index < inputs.length - 1) {
+				setTimeout(() => {
+					inputs[index + 1].focus();
+				}, 1);
+			} else if (index == inputs.length - 1) {
+				setTimeout(() => {
+					button.focus();
+				}, 1);
+			}
+		}
+	});
+});
