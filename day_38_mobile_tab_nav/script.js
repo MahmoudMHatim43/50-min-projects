8a805c0d2e3c0be73fbf04e587ca77a1649cf8d4
MahmoudMHatim43 <mahmoudmhatim@gmail.com>
2024-07-01 12:00:57 +0300

    ✔✔✔

--- day_38_mobile_tab_nav/script.js
+++ day_38_mobile_tab_nav/script.js
@@ -3,9 +3,13 @@ const pages = document.querySelectorAll("img");
 
 buttons.forEach((button, index) => {
 	button.addEventListener("click", () => {
+		buttons.forEach(button => {
+			button.classList.remove("active");
+		});
 		pages.forEach(page => {
 			page.classList.remove("show");
 		});
+		button.classList.add("active");
 		pages[index].classList.add("show");
 	});
 });
