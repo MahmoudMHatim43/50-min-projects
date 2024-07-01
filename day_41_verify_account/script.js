9553324936770f2ea51081387ca5b3eb04ee13db
MahmoudMHatim43 <mahmoudmhatim@gmail.com>
2024-07-01 22:08:36 +0300

    ✔✔✔

--- day_41_verify_account/script.js
+++ day_41_verify_account/script.js
@@ -9,6 +9,7 @@ inputs.forEach((input, index) => {
 	input.addEventListener("keydown", event => {
 		if (event.key >= 0 && event.key < 10) {
 			if (index < inputs.length - 1) {
+				inputs[index].value = ``;
 				setTimeout(() => {
 					inputs[index + 1].focus();
 				}, 1);
@@ -17,6 +18,10 @@ inputs.forEach((input, index) => {
 					button.focus();
 				}, 1);
 			}
+		} else if (event.key == "Backspace") {
+			setTimeout(() => {
+				inputs[index - 1].focus();
+			}, 1);
 		}
 	});
 });
